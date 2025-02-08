7afe8494e19b58603a0121fb2bacac9b4d17168f
MomotekD <86006101+MomotekD@users.noreply.github.com>
2025-02-08 19:16:25 +0100

    Add basic arithmetic functions and some of query selectors for calculator.

---
+++ script.js
@@ -0,0 +1,21 @@
+const display = document.querySelector(".display-label");
+const buttons = document.querySelectorAll(".btn");
+
+
+
+const add = function add(a, b){
+    return a + b;
+}
+
+ const substract = function substract(a, b){
+    return a - b;
+}
+
+const multiply = function multiply(a, b){
+    return a * b;
+}
+
+const divide = function divide(a, b){
+    return a / b;
+}
+
